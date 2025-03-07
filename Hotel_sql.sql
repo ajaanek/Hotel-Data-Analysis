b81da35daafce50f7bee919d4ae74080791350e2
ajaanek <122136739+ajaanek@users.noreply.github.com>
2025-03-07 18:58:53 -0500

    Add sql file

---
+++ Hotel_sql.sql
@@ -0,0 +1,17 @@
+
+-- Joining tables into 1 
+WITH hotels AS 
+(
+    SELECT * FROM dbo.['2018$']
+    UNION
+    SELECT * FROM dbo.['2019$']
+    UNION
+    SELECT * FROM dbo.['2020$']
+)
+SELECT * 
+INTO dbo.hotels_staging  
+FROM hotels
+LEFT JOIN dbo.market_segment$ 
+    ON hotels.market_segment = market_segment$.market_segment
+LEFT JOIN dbo.meal_cost$ 
+    ON meal_cost$.meal = hotels.meal;
